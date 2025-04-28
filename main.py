c2982ae48bbe019b3136ec384fcf5ba932fd8a0e
mahmudurmahid <m.mahid@icloud.com>
2025-04-28 14:24:25 +0100

    store data about events in data.txt file

--- main.py
+++ main.py
@@ -15,12 +15,34 @@ def scrape(url):
 
 
 def extract(source):
+    """ Extract the tour data from the page source"""
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["tours"]
     return value
+
+
+def send_email():
+    print("Email sent successfully!")
+
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
 
 
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+    
+    content = read(extracted)
+
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
