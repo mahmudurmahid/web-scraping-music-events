13b13387509a6e3a4b91e0e4a16c51d1dc186325
mahmudurmahid <m.mahid@icloud.com>
2025-04-29 11:56:54 +0100

    changed where data is added from a text file to an sqlite database

--- main.py
+++ main.py
@@ -3,21 +3,20 @@ import selectorlib
 import smtplib, ssl
 import os
 import time
+import sqlite3
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
-
-"Insert INTO events VALUES ('Tigers', 'Tiger City', '2088.10.14')"
-"SELECT * FROM events WHERE date='2088.10.15'"
-
-
-
+# Web scraping URL and headers
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
     }
+
+# Establish a connection to the SQLite database
+connection = sqlite3.connect("data.db")
 
 
 def scrape(url):
@@ -48,18 +47,30 @@ def send_email(message):
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
+    print("Email sent successfully!")
 
 
 def store(extracted):
     """ Store the extracted data in a text file by appending"""
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
+    connection.commit()
 
 
 def read(extracted):
-    """ Read the content of the text file"""
-    with open("data.txt", "r") as file:
-        return file.read()
+    """ Read the content of the SQL file"""
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    row = cursor.fetchall()
+    print(row)
+    return row
 
 
 if __name__ == "__main__":
@@ -68,9 +79,9 @@ if __name__ == "__main__":
         extracted = extract(scraped)
         print(extracted)
         
-        content = read(extracted)
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message= f"New tour available: {extracted}")
         time.sleep(2)
